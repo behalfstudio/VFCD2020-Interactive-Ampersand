b45b37a2a2230162102c67bc782df5a016f804a2
Behalf Studio <us@onbehalfof.studio>
2020-10-24 18:18:27 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -85,8 +85,8 @@ function declareConstants() {
   MAX_FORCE_RATIO = 0.025;
   CLOSE_ENOUGH_TARGET = GRID_UNIT * 7;
 
-  BASE_FORCE = GRID_UNIT;
-  FALLOFF = 3;
+  BASE_FORCE = GRID_UNIT / 2;
+  FALLOFF = 8;
 
   WHITE = color(255);
   BLACK = color(0);
